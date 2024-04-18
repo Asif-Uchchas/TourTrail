89ade8aeb274d1ee5e266dc089ddda119e883383
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-04-18 21:44:33 +0600

    Created the modal for register functionalities

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-2xl text-teal-500">
+    <div className="text-2xl text-teal-500 text-center">
       Hello TourTrail!
     </div>
   );

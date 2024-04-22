8bd3be92ee30a3a2466407b00a03fb941703c47d
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2024-04-22 21:59:29 +0600

    Images of the hotel rooms can be uploaded successfully, Image upload and database works

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from 'react-hook-form'
 import CountrySelect from '../inputs/CountrySelect'
 import dynamic from 'next/dynamic'
 import Counter from '../inputs/Counter'
+import ImageUpload from '../inputs/ImageUpload'
 
 
 enum STEPS{
@@ -55,6 +56,7 @@ const RentModal = () => {
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc');
 
     const Map = useMemo(()=>dynamic(()=> import('../Map'),{
       ssr:false
@@ -158,6 +160,21 @@ const RentModal = () => {
           subtitle='How many bathrooms do you have?'
           value={bathroomCount}
           onChange={(value)=>setCustomeValue("bathroomCount",value)}
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.IMAGES){
+    bodyContent=(
+      <div className='flex flex-col gap-8'>
+        <Heading
+        title='Add a Photo of the room'
+        subtitle='Show guests how the room would look like!'
+        />
+        <ImageUpload 
+        value={imageSrc}
+        onChange={(value)=>setCustomeValue("imageSrc",value)}
         />
       </div>
     )

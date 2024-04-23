e9f6d3ac3db3f77a1ebb4cf64c78bcc2c699a27a
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2024-04-24 03:21:17 +0600

    Favourite Function Works

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { SafeUser } from '../types';
+import useFavorite from '../hooks/useFavorite';
 
 
 interface HeartButtonProps{
@@ -11,17 +12,19 @@ interface HeartButtonProps{
 
 const HeartButton = ({ listingId, currentUser }: HeartButtonProps) => {
 
-    const hasFavourited = false
-    const toggleFavourite = () => { }
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser
+    })
 
 
   return (
       <div
-          onClick={toggleFavourite}
+          onClick={toggleFavorite}
           className=' relative hover:opacity-80 transition cursor-pointer'
       >
           <AiOutlineHeart size={28} className='fill-white absolute -top-[2px] -right-[2px]' />
-          <AiFillHeart size={24} className={hasFavourited ? 'fill-rose-500' : 'fill-neutral-500/70' } />
+          <AiFillHeart size={24} className={hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70' } />
     </div>
   );
 }

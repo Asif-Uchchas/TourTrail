2d9d9d8c59277a2c1d4a88cc6d328bf374841b8b
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2024-04-24 13:32:18 +0600

    Individual Listing Done

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing: any) =>{
+          {listings.map((listing) =>{
             return(
               <ListingCard
                 currentUser= {currentUser}

4398d25f741b3b85116c4aacf22a84dccfa3546a
Shahil <shahilyasarhaque@gmail.com>
2024-04-25 12:56:03 +0600

    Reservation screen done

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -44,7 +44,16 @@ const ReservationsClient = ({
         title='Reservations'
         subtitle='Bookings on your properties'
       />
-      <div className=' mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+      <div className=' 
+      mt-10 
+      grid 
+      grid-cols-1 
+      sm:grid-cols-2 
+      md:grid-cols-3 
+      lg:grid-cols-4 
+      xl:grid-cols-5 
+      2xl:grid-cols-6 
+      gap-8'>
         {reservations.map((reservation) => (
           <ListingCard
             key={reservation.id}
